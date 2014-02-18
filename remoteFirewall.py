702d5d741f5a7925e43c922039fd687081773495
communistwatermelon <zyko101@gmail.com>
2014-02-17 18:50:27 -0800

    fixed everything, remember to restart em1 through gui after running firewall

--- remoteFirewall.py
+++ remoteFirewall.py
@@ -65,30 +65,30 @@ def setupForwarding():
 def createUserChains():
 	print("Creating User Chains")
 	os.system("iptables -N TCP")
-	os.system("iptables -N UserTCP")
+	#os.system("iptables -N UserTCP")
 	os.system("iptables -A TCP")
-	os.system("iptables -A UserTCP")
+	#os.system("iptables -A UserTCP")
 	
 	os.system("iptables -N UDP")
-	os.system("iptables -N UserUDP")
+	#os.system("iptables -N UserUDP")
 	os.system("iptables -A UDP")	
-	os.system("iptables -A UserUDP")
+	#os.system("iptables -A UserUDP")
 
 	os.system("iptables -N ICMP")
-	os.system("iptables -N UserICMP")
+	#os.system("iptables -N UserICMP")
 	os.system("iptables -A ICMP")
-	os.system("iptables -A UserICMP")
+	#os.system("iptables -A UserICMP")
 
 def addUserChains():
 	print("Linking User Chains")
 	os.system("iptables -A FORWARD -p tcp -j TCP")
-	os.system("iptables -A TCP -p tcp -j UserTCP")
+	#os.system("iptables -A TCP -p tcp -j UserTCP")
 	
 	os.system("iptables -A FORWARD -p udp -j UDP")
-	os.system("iptables -A UDP -p udp -j UserUDP")
+	#os.system("iptables -A UDP -p udp -j UserUDP")
 	
 	os.system("iptables -A FORWARD -p icmp -j ICMP")
-	os.system("iptables -A ICMP -p icmp -j UserICMP")
+	#os.system("iptables -A ICMP -p icmp -j UserICMP")
 
 def firewallInit():
 	print("Initializing Firewall")
@@ -99,6 +99,7 @@ def firewallInit():
 
 	#Dropping Suspicious IP Addresses
 	os.system("iptables -A INPUT -s " + internalIP + " -i " + inputInt + " -j DROP")
+	os.system("iptables -A FORWARD -s " + internalIP + " -i " + inputInt + " -j DROP")
 
 	#Drops specifical edge-cases
 	os.system("iptables -A TCP -p tcp --sport 0:1024 --dport 80 -j DROP")
@@ -116,15 +117,15 @@ def firewallInit():
 	os.system("iptables -A TCP -p tcp --dport 23 -j DROP")
 
 	#SSH Delay & FTP Throughput
-	os.system("iptables -A PREROUTING -t mangle -p tcp --sport 22 -j TOS --set-tos Minimize-Delay")
-	os.system("iptables -A PREROUTING -t mangle -p tcp --sport 21 -j TOS --set-tos Minimize-Delay")
-	os.system("iptables -A PREROUTING -t mangle -p tcp --sport 20 -j TOS --set-tos Maximize-Throughput")
+	#os.system("iptables -A PREROUTING -t mangle -p tcp --sport 22 -j TOS --set-tos Minimize-Delay")
+	#os.system("iptables -A PREROUTING -t mangle -p tcp --sport 21 -j TOS --set-tos Minimize-Delay")
+	#os.system("iptables -A PREROUTING -t mangle -p tcp --sport 20 -j TOS --set-tos Maximize-Throughput")
 
 def dnsSetup():
 	print("DNS Setup")
-	os.system("iptables -A INPUT -p udp --sport 53 -m state --state ESTABLISHED -j ACCEPT")
-	os.system("iptables -A OUTPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT")
-	os.system("iptables -A INPUT -p udp --dport 67:68 -j ACCEPT")
+	#os.system("iptables -A INPUT -p udp --sport 53 -m state --state ESTABLISHED -j ACCEPT")
+	#os.system("iptables -A OUTPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT")
+	#os.system("iptables -A INPUT -p udp --dport 67:68 -j ACCEPT")
 
 	os.system("iptables -A FORWARD -i " + inputInt  + " -o " + outputInt + " -p udp --sport 53 -m state --state ESTABLISHED -j ACCEPT")
 	os.system("iptables -A FORWARD -i " + outputInt + " -o " + inputInt  + " -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT")
@@ -132,34 +133,36 @@ def dnsSetup():
 
 def enableTCPPortIn(port):
 	print("In Forwarding TCP Port " + port)
-	os.system("iptables -A UserTCP -i " + inputInt  + " -o " + outputInt + " -p tcp --sport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
-	os.system("iptables -A UserTCP -i " + outputInt + " -o " + inputInt  + " -p tcp --dport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A TCP -i " + inputInt  + " -o " + outputInt + " -p tcp --sport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A TCP -i " + outputInt + " -o " + inputInt  + " -p tcp --dport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
 	os.system("iptables -t nat -A PREROUTING -i " + inputInt + " -p tcp --dport " + port + " -j DNAT --to 192.168.10.42")
 
 def enableTCPPortOut(port):
 	print("Out Forwarding TCP Port " + port)
-	os.system("iptables -A UserTCP -i " + outputInt + " -o " + inputInt  + " -p tcp --sport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
-	os.system("iptables -A UserTCP -i " + inputInt  + " -o " + outputInt + " -p tcp --dport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A TCP -i " + outputInt + " -o " + inputInt  + " -p tcp --sport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A TCP -i " + inputInt  + " -o " + outputInt + " -p tcp --dport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
 
 def enableUDPPortIn(port):
 	print("In Forwarding UDP Port " + port)	
-	os.system("iptables -A UserUDP -o " + outputInt + " -i " + inputInt + " -p udp --sport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
-	os.system("iptables -A UserUDP -o " + inputInt + "  -i " + outputInt + " -p udp --dport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A UDP -o " + outputInt + " -i " + inputInt + " -p udp --sport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A UDP -o " + inputInt + "  -i " + outputInt + " -p udp --dport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -t nat -A PREROUTING -i " + inputInt + " -p udp --dport " + port + " -j DNAT --to 192.168.10.42")
 
 def enableUDPPortOut(port):
 	print("Out Forwarding UDP Port " + port)
-	os.system("iptables -A UserUDP -o " + outputInt + " -i " + inputInt + " -p udp --dport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
-	os.system("iptables -A UserUDP -o " + inputInt + "  -i " + outputInt + " -p udp --sport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A UDP -o " + outputInt + " -i " + inputInt + " -p udp --dport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A UDP -o " + inputInt + "  -i " + outputInt + " -p udp --sport " + port + " -m state --state NEW,ESTABLISHED -j ACCEPT")
 	
 def enableICMPIn(itype):
 	print("In Forwarding ICMP type " + itype)
-	os.system("iptables -A UserICMP -i " + inputInt + " -o " + outputInt + " -p icmp --icmp-type " + itype + " -m state --state NEW,ESTABLISHED -j ACCEPT")
-	os.system("iptables -A UserICMP -i " + outputInt + "  -o " + inputInt + " -p icmp --icmp-type " + itype + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A ICMP -i " + inputInt + " -o " + outputInt + " -p icmp --icmp-type " + itype + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A ICMP -i " + outputInt + "  -o " + inputInt + " -p icmp --icmp-type " + itype + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+#	os.system("iptables -t nat -A PREROUTING -i " + inputInt + " -p icmp " + itype + " -j DNAT --to 192.168.10.42")
 
 def enableICMPOut(itype):
 	print("Out Forwarding ICMP type " + itype)
-	os.system("iptables -A UserICMP -i " + outputInt + " -o " + inputInt + " -p icmp --icmp-type " + itype + " -m state --state NEW,ESTABLISHED -j ACCEPT")
-	os.system("iptables -A UserICMP -i " + inputInt + "  -o " + outputInt + " -p icmp --icmp-type " + itype + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A ICMP -i " + outputInt + " -o " + inputInt + " -p icmp --icmp-type " + itype + " -m state --state NEW,ESTABLISHED -j ACCEPT")
+	os.system("iptables -A ICMP -i " + inputInt + "  -o " + outputInt + " -p icmp --icmp-type " + itype + " -m state --state NEW,ESTABLISHED -j ACCEPT")
 
 def main():
 	while True:
@@ -211,7 +214,7 @@ def main():
 			for i in icmpTypesOut:
 				enableICMPOut(i)
 
-			os.system("iptables -A INPUT -p tcp --syn -j DROP")
+			#os.system("iptables -A INPUT -p tcp --syn -j DROP")
 			os.system("iptables -A FORWARD -o " + inputInt  + " -i " + outputInt + " -p tcp --syn -j DROP")
 
 			dnsSetup()
