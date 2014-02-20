4cbc8cd17bc17a26974924027a7a60cd242ab003
communistwatermelon <zyko101@gmail.com>
2014-02-19 21:45:55 -0800

    Added testing scripts

---
+++ testingExternal.py
@@ -0,0 +1,82 @@
+'''
+--------------------------------------------------------------------------------------------
+--	SCRIPT:	    testingInternal.py
+--
+--	FUNCTIONS:  None
+--
+--	DATE:       February 19, 2014
+--
+--	DESIGNERS:  John Payment
+--              Jake Miner
+--
+--	PROGRAMMER: Jake Miner
+--                  
+--
+--	NOTES: This program tests the validity of the firewall rules from outside the firewall
+-- 			It is to be run on the network outside the firewall. 
+--			PLEASE SET THE FIREWALL IP
+---------------------------------------------------------------------------------------------
+'''
+firewallIP = "192.168.0.xx"
+clientIP   = "192.168.10.42" 
+
+#Drop All packets with a destination address of the firewall machine that arrive from outside. 
+os.system("hping " + firewallIP + " -S -V")
+os.system("hping " + firewallIP + " -p 22 -V")
+os.system("hping " + firewallIP + " -p 80 -V")
+os.system("hping " + firewallIP + " -p 443 -V")
+
+#Drop all packets with ACK bit set which arrive from outside and are using non-allowed ports.
+os.system("hping " + clientIP + " -p 12332 -S -V")
+os.system("hping " + clientIP + " -p 12332 -V")
+
+#Drop all Stateless packets.
+os.system("hping " + clientIP + " -p 80 -V")
+os.system("hping " + clientIP + " -p 22 -V")
+os.system("hping " + clientIP + " -p 33 -V")
+
+#Drop all packets arriving from outside but with the source address of an interior machine.
+os.system("hping " + clientIP + " -a 192.168.10.12 -p 22 -S -V")
+os.system("hping " + clientIP + " -a 192.168.10.23 -p 22 -V")
+os.system("hping " + clientIP + " -a 192.168.10.5 -p 80 -S -V")
+
+#Drop incoming TCP packets connecting from port 0
+os.system("hping " + clientIP + " -s 0 -p 22 -V")
+os.system("hping " + clientIP + " -s 0 -p 80 -V")
+
+#Drop incoming UDP packets connecting from port 0
+os.system("hping " + clientIP + " -s 0 -p 80 -2 -V")
+os.system("hping " + clientIP + " -s 0 -p 22 -2 -V")
+
+#Drop packets connecting to port 80 from ports between 0 and 1024
+os.system("hping " + clientIP + " -p 80 -s 1022 -V")
+os.system("hping " + clientIP + " -p 80 -s 2 -V")
+
+#Drop packets with the SYN bit set which are trying to connect to high-number ports
+os.system("hping " + clientIP + " -p 12252 -S -V")
+os.system("hping " + clientIP + " -p 1025 -S -V")
+
+#Drop outgoing packets that are using telnet (TCP port 23)
+os.system("hping " + clientIP + " -p 23 -S -V")
+os.system("hping " + clientIP + " -p 23 -V")
+os.system("hping " + clientIP + " -p 23 -s 23 -S -V")
+os.system("hping " + clientIP + " -p 23 -s 23 -V")
+os.system("hping " + clientIP + " -s 23 -S -V")
+os.system("hping " + clientIP + " -s 23 -V")
+
+#Drop packets from outside with destination ports in the range of 32768 - 32775
+os.system("hping " + clientIP + " -p 32768 -S -V")
+os.system("hping " + clientIP + " -p 32768 -V")
+os.system("hping " + clientIP + " -p 32775 -S -V")
+os.system("hping " + clientIP + " -p 32775 -V")
+
+#Drop TCP packets from outside with destination port 111
+os.system("hping " + clientIP + " -p 111 -S -V")
+os.system("hping " + clientIP + " -p 111 -V")
+
+#Drop TCP packets from outside with destination port 515
+os.system("hping " + clientIP + " -p 515 -S -V")
+os.system("hping " + clientIP + " -p 515 -V")
+
+#Can connect to interior PC using SSH from outside.
+os.system("ssh " + clientIP)
---
+++ testingInternal.py
@@ -0,0 +1,68 @@
+'''
+--------------------------------------------------------------------------------------------
+--	SCRIPT:	    testingInternal.py
+--
+--	FUNCTIONS:  None
+--
+--	DATE:       February 19, 2014
+--
+--	DESIGNERS:  John Payment
+--              Jake Miner
+--
+--	PROGRAMMER: Jake Miner
+--                  
+--
+--	NOTES: This program tests the validity of the firewall rules from behind the firewall
+-- 			It is to be run on the client behind the firewall. 
+--			PLEASE SET THE FIREWALL IP
+---------------------------------------------------------------------------------------------
+'''
+firewallIP = "192.168.0.xx"
+clientIP   = "192.168.10.42" 
+outgoingIP = "192.168.0.12"
+
+#Can successfully complete a DNS lookup.
+os.system("host(http://google.com)")
+
+#Drop outgoing TCP packets connecting from port 0
+os.system("hping " + outgoingIP + " -s 0 -p 22 -V")
+os.system("hping " + outgoingIP + " -s 0 -p 80 -V")
+
+#Drop outgoing UDP packets connecting from port 0
+os.system("hping " + outgoingIP + " -s 0 -p 80 -2 -V")
+os.system("hping " + outgoingIP + " -s 0 -p 22 -2 -V")
+
+
+#Drop packets connecting to port 80 from ports between 0 and 1024
+os.system("hping " + outgoingIP + " -p 80 -s 1022 -V")
+os.system("hping " + outgoingIP + " -p 80 -s 2 -V")
+
+#Drop packets with the SYN bit set which are trying to connect to high-number ports
+os.system("hping " + outgoingIP + " -p 12252 -S -V")
+os.system("hping " + outgoingIP + " -p 1025 -S -V")
+
+#Drop incoming packets that are using telnet (TCP port 23)
+os.system("hping " + outgoingIP + " -p 23 -S -V")
+os.system("hping " + outgoingIP + " -p 23 -V")
+os.system("hping " + outgoingIP + " -p 23 -s 23 -S -V")
+os.system("hping " + outgoingIP + " -p 23 -s 23 -V")
+os.system("hping " + outgoingIP + " -s 23 -S -V")
+os.system("hping " + outgoingIP + " -s 23 -V")
+
+#Drop all TCP packets with both the SYN and FIN bits set
+os.system("hping " + outgoingIP + " -p 22 -S -F -V")
+os.system("hping " + outgoingIP + " -p 80 -S -F -V")
+
+#Can connect to an external http server
+os.system("firefox linux.com")
+
+#Can connect to a external HTTPS server
+os.system("firefox google.com")
+
+#Can connect to an external PC using SSH from inside.
+os.system("ssh 192.168.0.3")
+
+#Allowed ICMP Types are forwarded. 
+os.system("google.com")
+os.system(outgoingIP)
+
